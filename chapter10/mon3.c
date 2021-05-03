db2729c7254c6ec0aac7d8131e978142a5ffaed6
zhouyan <2396411660@qq.com>
2021-05-03 13:41:24 +0800

    add some pointer exercise

---
+++ chapter10/mon3.c
@@ -0,0 +1,12 @@
+/*day_mon3.c*/
+#include <stdio.h>
+#define MONTHS 12
+int main(void)
+{
+	int days[MONTHS] = {31,28,31,30,31,30,31,31,30,31,30,31};
+	int index;
+
+	for (index = 0;index<MONTHS;index++)
+		printf("Month %2d has %d days.\n",index+1,*(days+index));
+	return 0;
+}
---
+++ chapter10/zippo1.c
@@ -0,0 +1,17 @@
+/*zippo1.c*/
+#include<stdio.h>
+int main(void)
+{
+  int zippo[4][2] = {{2,4},{6,8},{1,3},{5,7}};
+  printf("zippo = %p,zippo+1 = %p\n",zippo,zippo+1);
+  printf("zippo[0] = %p,zippo[0]+1=%p\n",zippo[0],zippo[0]+1);
+  printf("*zippo = %p,*zippo+1 = %p\n",*zippo,*zippo+1);
+  printf("zippo[0][0] = %d\n",zippo[0][0]);
+  printf("*zippo[0] = %d\n",*zippo[0]);
+  printf("**zippo = %d\n",**zippo);
+  printf("zippo[2][1] = %d\n",zippo[2][1]);
+  printf("*(*(zippo+2)+1) = %d\n",*(*(zippo+2)+1));
+  printf("add some test\n");
+  printf("*(*zippo+3) = %d\n",*(*zippo+3));
+  return 0;
+}
